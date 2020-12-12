c405895a49e69e2f476ed3a221d3e0e0179c5713
luodx <luodx@lovengame.com>
2020-12-12 17:46:19 +0800

    init project

---
+++ src/webgl/mesh-renderer.ts
@@ -0,0 +1,28 @@
+export default class MeshRenderer {
+    protected gl:any
+
+    constructor() {
+
+    }
+
+    public setGL(gl:any) {
+        this.gl = gl
+    }
+
+    public setTexture() {
+
+    }
+
+    public setMesh() {
+
+    }
+
+    public setShader() {
+
+    }
+
+    public render() {
+
+    }
+
+}
